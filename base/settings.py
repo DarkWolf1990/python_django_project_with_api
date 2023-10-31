57f3d02b9875bff2bcb1f04cde427af513ffd3a3
darkwolf <darkwolf1990janary@gmail.com>
2023-10-31 18:11:35 +0300

    update project

--- base/settings.py
+++ base/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from . import models
 
-# Create your views here.
+
+def index(request):
+    courses = models.Course.objects.all()
+    return render(request, 'shop/courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    try:
+        course = models.Course.objects.get(pk=course_id)
+        return render(request, 'shop/single_course.html', {'course': course})
+    except models.Course.DoesNotExist:
+        raise Http404()
